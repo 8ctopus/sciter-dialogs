95b78c05c6c673d99cb60c4e507bfa9ed41b0ddf
8ctopus <hello@octopuslabs.io>
2021-07-08 14:45:46 +0500

    improved: bring scapp to top and set focus

--- js/reload.js
+++ js/reload.js
@@ -1,3 +1,12 @@
+document.on("ready", function() {
+    // bring window to front
+    Window.this.isTopmost = true;
+    Window.this.isTopmost = false;
+
+    // set focus
+    document.body.state.focus = true;
+});
+
 // add scapp refresh
 document.on("keydown", function(event, element) {
     if (event.code !== "KeyF5")
