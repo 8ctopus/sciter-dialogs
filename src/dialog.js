a130c679c80f8538e5bd003cc81bbaee8b123af1
8ctopus <hello@octopuslabs.io>
2021-07-08 10:32:44 +0500

    improved: dialogs turned into a class

--- src/dialog.js
+++ src/dialog.js
@@ -1,25 +1,28 @@
-/**
- * Show modal dialog
- * @param  string type
- * @param  string message
- * @param  string buttons
- * @param (optional) options
- * @return string result
- */
-export function showDialog(type, message, buttons, options)
+export class dialog
 {
-    return Window.this.modal({
-        type: Window.POPUP_WINDOW,
-        url: __DIR__ + "dialog.htm",
-        state: Window.WINDOW_SHOWN,
-        alignment: 5,
-//        width: 400,
-//        height: 170,
-        parameters: {
-            type: type,
-            message: message,
-            buttons: buttons,
-            options: options,
-        }
-    });
+    /**
+     * Show modal dialog
+     * @param  string type
+     * @param  string message
+     * @param  string buttons
+     * @param (optional) options
+     * @return string result
+     */
+    static show(type, message, buttons, options)
+    {
+        return Window.this.modal({
+            type: Window.POPUP_WINDOW,
+            url: __DIR__ + "dialog.htm",
+            state: Window.WINDOW_SHOWN,
+            alignment: 5,
+    //        width: 400,
+    //        height: 170,
+            parameters: {
+                type: type,
+                message: message,
+                buttons: buttons,
+                options: options,
+            }
+        });
+    }
 }
