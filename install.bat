a4acd833b30338198513db11c86ec1df8a4e5127
8ctopus <hello@octopuslabs.io>
2021-07-08 15:27:30 +0500

    fixed: package manager download

--- install.bat
+++ install.bat
@@ -4,6 +4,6 @@ curl -LO https://github.com/c-smile/sciter-js-sdk/raw/051fa33e850a45fd2b90a36f52
 curl -LO https://github.com/c-smile/sciter-js-sdk/raw/051fa33e850a45fd2b90a36f5244cc401892dbe9/bin/windows/x32/inspector.exe
 
 REM sciter package manager 0.1.2
-curl -LO https://github.com/8ctopus/sciter-package-manager/releases/download/0.1./spm.phar
+curl -LO https://github.com/8ctopus/sciter-package-manager/releases/download/0.1.2/spm.phar
 
 pause
