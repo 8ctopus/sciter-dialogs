b332bc91b527463b7cac252232c55aeb92fe54d2
8ctopus <hello@octopuslabs.io>
2021-07-08 10:45:30 +0500

    added: class options that apply to all dialogs

--- src/dialogs.js
+++ src/dialogs.js
@@ -1,15 +1,30 @@
 export class dialogs
 {
+    static #options = [];
+
+    /**
+     * Set options
+     * @param object options
+     */
+    static setOptions(options)
+    {
+        this.#options = options;
+    }
+
     /**
      * Show modal dialog
      * @param  string type
      * @param  string message
      * @param  string buttons
-     * @param (optional) options
+     * @param (optional) object options
      * @return string result
      */
     static show(type, message, buttons, options)
     {
+        // set options if not defined
+        if (typeof options === "undefined")
+            options = this.#options;
+
         return Window.this.modal({
             type: Window.POPUP_WINDOW,
             url: __DIR__ + "dialog.htm",
