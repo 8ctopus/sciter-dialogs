452021b0ace853bbe1757e40a79771922aaff3d7
8ctopus <hello@octopuslabs.io>
2021-10-13 15:48:33 +0500

    updated: sciter sdk to 4.4.8.14

--- install.bat
+++ install.bat
@@ -2,10 +2,10 @@ mkdir bin\win-x32
 
 cd bin\win-x32
 
-REM sciter 4.4.8.13
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/windows/x32/scapp.exe
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/windows/x32/inspector.exe
-curl -LO https://github.com/c-smile/sciter-js-sdk/raw/f810da6a1887220e5d7e9d5a9ec6176508967f0d/bin/windows/x32/sciter.dll
+REM sciter 4.4.8.14
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/b5c4d4efe4ed48fc96c7900c8e89e18c3c1c6397/bin/windows/x32/scapp.exe
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/b5c4d4efe4ed48fc96c7900c8e89e18c3c1c6397/bin/windows/x32/inspector.exe
+curl -LO https://github.com/c-smile/sciter-js-sdk/raw/b5c4d4efe4ed48fc96c7900c8e89e18c3c1c6397/bin/windows/x32/sciter.dll
 
 cd ..
 
